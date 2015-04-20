4222785914b6f1d5d78107bb84dc919b921000d4
y1275963 <1275963@gmail.com>
2015-04-20 09:42:10 +0100

    sampling

--- main.m
+++ main.m
@@ -1,5 +1,18 @@
-N = 50;
+clear all;
 
+N = 50;
+data_rate = 1000
 % creating N bits, the bit rate is 1000bps(1000 bits per second)
 
 data = randint(N,1);
+Fs = 8000; % sampling frequency
+
+samplesPerBit  = Fs/data_rate;
+
+k=1;
+for i = 1:N
+    for j = 1:samplesPerBit  % these loops create the
+        y(k) = data(i);      % bit stream
+        k=k+1;
+    end
+end  
