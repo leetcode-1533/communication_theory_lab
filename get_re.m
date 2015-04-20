e8ebc01a215f614273f024874bb5462d92f8a48d
y1275963 <1275963@gmail.com>
2015-04-20 13:17:07 +0100

    Monte Carlo simulations

--- get_re.m
+++ get_re.m
@@ -1,5 +1,10 @@
 for index = 0:1:12
-    tk(index+1) = main(index)
+    for tis = 1:1:100
+        temp = [];
+        temp = [temp,main(index)];
+        val = mean(temp);
+    end
+    tk(index+1) = val;
 end
 
 re = [0:1:12;tk];
